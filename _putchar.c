b6959cda37923115385b63ec1907c9fd86dac836
eshon2k <shongaec@gmail.com>
2023-04-04 18:08:10 -0700

    _putchar.c

--- _putchar.c
+++ _putchar.c
@@ -1,28 +1,30 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _putchar - print char with stdout
+ * @ch: char to print
+ * Return: Output.
  */
-int _putchar(char c)
+int _putchar(char ch)
 {
-	return (write(1, &c, 1));
-}
+	static int contador;
+	static char buffer[1024];
 
-
-/**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
- * Return: number of chars written
- */
-int _puts(char *str)
-{
-	register short i;
-
-	for (i = 0; str[i]; i++)
-		_putchar(str[i]);
-	return (i);
+	if (ch == -1)
+	{
+		contador = 0;
+		return (0);
+	}
+	if (ch == -2 || contador == 1024)
+	{
+		write(1, buffer, contador);
+		contador = 0;
+	}
+	if (ch != -1 && ch != -2)
+	{
+		buffer[contador] = ch;
+		contador++;
+		return (1);
+	}
+	return (0);
 }
